433ced46b47e29a6e9470892769bba2288cc97bf
high489 <vitalii.matskaniuk@gmail.com>
2025-04-17 01:48:31 -0300

    Update: The default Next.js styling has been reset
    The default Next.js styling has been reset
    Add normalize.css
    Add weather favicon
    Add few global styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
+import 'normalize.css'
 import '@/styles/index.scss';
-//import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -29,5 +30,5 @@ export default function RootLayout({
         {children}
       </body>
     </html>
-  );
+  )
 }
