635a293025748483d0f4380bb62ede151f17b471
high489 <vitalii.matskaniuk@gmail.com>
2025-04-21 13:43:05 -0300

    Fix: date format for all browsers

--- src/shared/utils/format-time.ts
+++ src/shared/utils/format-time.ts
@@ -1,8 +1,19 @@
 export const hHmMformatTime = (date: Date | string): string => {
-  const time = new Date(date).toLocaleTimeString('en-US', {
+  if (typeof date === 'string') {
+    date = date.replace(' ', 'T')
+  }
+  
+  const validDate = new Date(date);
+  
+  if (isNaN(validDate.getTime())) {
+    return 'Invalid Time'
+  }
+
+  const time = validDate.toLocaleTimeString('en-US', {
     hour: 'numeric',
     minute: '2-digit',
     hour12: false,
-  })
+  });
+  
   return time.replace(/^0/, '')
-}
+};
